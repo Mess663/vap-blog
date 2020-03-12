f94e715d3ead9de5433654ed903fb7a65364fcac
liukunhui <liukunhui@youcheyihou.com>
2020-03-12 22:05:14 +0800

    chore(web): 初始化web部分

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,15 @@
 FROM golang:latest
 WORKDIR /home/git/www/blog
 ADD . /home/git/www/blog
+
+RUN curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+    && export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+       [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    && nvm i node
+    && nvm use node
 RUN go build .
+
 EXPOSE 8080
 ENTRYPOINT ["./blog"]
 CMD ["deploy.sh"]
+
--- main.go
+++ main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
+	"flag"
+	"html/template"
+	"log"
 	"net/http"
-
-	"github.com/labstack/echo"
 )
 
+var addr = flag.String("addr", ":8080", "http service address") // Q=17, R=18
+
+var templ = template.Must(template.New("qr").Parse(templateStr))
+
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, 第一次部署!")
-	})
-	e.Logger.Fatal(e.Start(":8080"))
+	flag.Parse()
+	http.Handle("/", http.HandlerFunc(QR))
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
+
+func QR(w http.ResponseWriter, req *http.Request) {
+	templ.Execute(w, req.FormValue("s"))
+}
+
+const templateStr = `
+<html>
+<head>
+<title>QR Link Generator</title>
+</head>
+<body>
+{{if .}}
+<img src="http://chart.apis.google.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl={{.}}" />
+<br>
+{{.}}
+<br>
+<br>
+{{end}}
+<form action="/" name=f method="GET"><input maxLength=1024 size=70
+name=s value="" title="Text to QR Encode"><input type=submit
+value="Show QR" name=qr>
+</form>
+</body>
+</html>
+`
