035a3dca24b047b10d9231a48b6b69b187f5d285
liukunhui <liukunhui@youcheyihou.com>
2020-03-14 14:41:24 +0800

    chore(webpack): 加入压缩

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-// const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
